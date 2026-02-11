05fbdee1288e211a539fdc0a209fd1c182e7bf63
Richard He <103625533+Richardwarriors@users.noreply.github.com>
2026-02-11 08:18:23 -0500

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 import numpy as np
@@ -46,8 +47,7 @@ class Scratch(object):
             self.loss_fn = 'point-wise'
             self.is_rmse = False
 
-    def train(self, train_data, test_data, active_test_data, inactive_test_data, verbose=2, save_dir='',
-              id=0, given_model=''):
+    def train(self, train_data, test_data, active_test_data, inactive_test_data, verbose=2, id=0, given_model=''):
         print('Using device:', self.device)
         # seed for reproducibility
         seed_all(self.seed)
@@ -76,6 +76,10 @@ class Scratch(object):
         total_time = 0
 
         pos_dict = np.load(self.pos_dir, allow_pickle=True).item()
+
+        best_user_emb = None
+        best_item_emb = None
+        best_model_state = None
 
         for t in range(self.epochs):
             # if verbose == 2:
@@ -115,9 +119,23 @@ class Scratch(object):
                 count_dec = 0
                 best_ndcg = test_ndcg
                 best_hr = test_hr
-                # if len(save_dir) > 0:
-                #     torch.save(model.state_dict(), save_dir + '/model' + '.pth')
-                #     torch.save(model.user_mat.weight.detach().cpu().numpy(), save_dir + '/user_mat' + '.npy')
+
+                #best model
+                best_model_state = {k: v.cpu().clone() for k, v in model.state_dict().items()}
+                
+                #best model user and item embedding
+                if self.model_type == 'neumf':
+                    best_user_emb = torch.cat(
+                        [model.user_mat_mlp.weight,
+                        model.user_mat_mf.weight],
+                        dim=1
+                    ).detach().cpu().clone()
+
+                    best_item_emb = torch.cat(
+                        [model.item_mat_mlp.weight,
+                        model.item_mat_mf.weight],
+                        dim=1
+                    ).detach().cpu().clone()
             else:
                 count_dec += 1
 
@@ -137,18 +155,36 @@ class Scratch(object):
                                               10,user_mapping,pos_mapping)
         print('inactive_test_ndcg:', inactive_ndcg)
         print('-------best--------')
+
+        # ===== save best model =====
+        save_dir = f'results/best_{self.model_type}_{self.n_user}_{self.n_item}'
+        os.makedirs(save_dir, exist_ok=True)
+
+        if best_model_state is not None:
+            torch.save(best_model_state, os.path.join(save_dir, 'best_model.pth'))
+            print("Best model saved.")
+
+        # ===== save best embedding =====
+        if self.model_type == 'neumf' and best_user_emb is not None:
+
+            user_dict = {}
+            item_dict = {}
+
+            user_np = best_user_emb.numpy()
+            item_np = best_item_emb.numpy()
+
+            for uid in range(user_np.shape[0]):
+                user_dict[np.int64(uid)] = user_np[uid:uid+1]
+
+            for iid in range(item_np.shape[0]):
+                item_dict[np.int64(iid)] = item_np[iid:iid+1]
+
+            np.save(os.path.join(save_dir, 'ml-1m_neumf_best_user_emb.npy'), user_dict)
+            np.save(os.path.join(save_dir, 'ml-1m_neumf_best_item_emb.npy'), item_dict)
+
+            print("Best embeddings saved.")
+
         result = {'time': total_time, 'ndcg': best_ndcg, 'hr': best_hr, 'active_ndcg': active_ndcg,
                   'active_hr': active_hr, 'inactive_ndcg': inactive_ndcg, 'inactive_hr': inactive_hr}
-
-        # save model
-        # torch.save(model.state_dict(), f'results/gowalla/{self.model_type}.pth')
-
-        # load mat
-        # user_mat = np.load('user_mat.npy')
-        # item_mat = np.load('item_mat.npy')
-
-        # np.save(save_dir + '/log.npy', self.log)
-        # load log
-        # log = np.load('log.npy', allow_pickle=True).item()
 
         return model, result
